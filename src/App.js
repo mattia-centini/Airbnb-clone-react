1feb7fbdbcdb2f5608314033d80b8da1ed953850
Mattia Centini <mattia.centini@gmail.com>
2020-10-12 22:10:48 +0200

    styled footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Footer />
       {/* Header */}
       {/* banner */}
       {/* Search*/}
